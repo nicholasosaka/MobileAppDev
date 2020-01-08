5522a584d016a2a95033b79beee85cd7534f4064
Nikko Osaka <nikko.osaka99@gmail.com>
2020-01-08 12:34:58 -0500

    Complete Question 1

--- IC01-1/edu/uncc/cci/mobileapps/MainPart1.java
+++ IC01-1/edu/uncc/cci/mobileapps/MainPart1.java
@@ -1,5 +1,9 @@
 package edu.uncc.cci.mobileapps;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MainPart1 {
     /*
     * Question 1:
@@ -13,8 +17,57 @@ public class MainPart1 {
     public static void main(String[] args) {
 
         //example on how to access the Data.users array.
+	    List<User> users = new ArrayList<>();
         for (String str : Data.users) {
-            System.out.println(str);
+            users.add(new User(str));
         }
+
+        Collections.sort(users);
+	    System.out.println("Printing top 10 oldest users:");
+	    for(int i = 0; i < 10; i++){
+		    System.out.println(users.get(i).toString());
+	    }
     }
+}
+
+class User implements Comparable<User>{
+	private String fname, lname, email, gender, city, state;
+	private int age;
+
+
+	User(String user){
+		String[] tokens = user.split(",");
+		for(int i = 0; i < tokens.length; i++){
+			String trimmedToken = tokens[i].trim();
+			switch(i){
+				case 0: fname = trimmedToken;
+					break;
+				case 1: lname = trimmedToken;
+					break;
+				case 2: age = Integer.parseInt(trimmedToken);
+					break;
+				case 3: email = trimmedToken;
+					break;
+				case 4: gender = trimmedToken;
+					break;
+				case 5: city = trimmedToken;
+					break;
+				case 6: state = trimmedToken;
+			}
+		}
+	}
+
+	@Override
+	public int compareTo(User user) {
+		return (user.getAge() - this.getAge());
+	}
+
+	@Override
+	public String toString() {
+		return fname + " " + lname + ", " + age + ", " + email + ", " + gender + ", " + city + ", " + state;
+	}
+
+	private int getAge() {
+		return age;
+	}
 }
