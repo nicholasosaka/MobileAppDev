66eb92552bcb9cd8cdbeb1f211831462eafa6554
Nikko Osaka <nikko.osaka99@gmail.com>
2020-01-12 23:50:11 -0500

    fields, toString, getters

--- IC01-1/edu/uncc/cci/mobileapps/StoreItem.java
+++ IC01-1/edu/uncc/cci/mobileapps/StoreItem.java
@@ -1,4 +1,33 @@
 package edu.uncc.cci.mobileapps;
 
 public class StoreItem {
+
+	private String name;
+	private int id;
+	private int price;
+
+	StoreItem(String str){
+		String[] arr = str.split(",");
+
+		this.name = arr[0];
+		this.id = Integer.parseInt(arr[1]);
+		this.price = Integer.parseInt(arr[2]);
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + getId() + "\tItem: " + getName() + "\tPrice: " + getPrice();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getPrice() {
+		return price;
+	}
 }
