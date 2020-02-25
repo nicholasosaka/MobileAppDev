6b6397f2e4a2f268a34ee52f7f6904560383ba20
Nikko Osaka <nikko.osaka99@gmail.com>
2020-02-25 12:11:00 -0500

    Track includes date

--- HW/HW02/app/src/main/java/com/example/hw02/Track.java
+++ HW/HW02/app/src/main/java/com/example/hw02/Track.java
@@ -10,6 +10,7 @@ public class Track {
     private String album;
     private double trackPrice;
     private double albumPrice;
+    private String date;
 
     public Track(JSONObject trackObject) {
         try {
@@ -19,19 +20,29 @@ public class Track {
             this.album = trackObject.getString("collectionName");
             this.trackPrice = trackObject.getDouble("trackPrice");
             this.albumPrice = trackObject.getDouble("collectionPrice");
+
+            String raw = trackObject.getString("releaseDate");
+            String[] datePieces = raw.substring(0,raw.indexOf("T")).split("-");
+
+            this.date = datePieces[1] + "-" + datePieces[2] + "-" + datePieces[0];
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
     }
 
-    public Track(String name, String genre, String artist, String album, double trackPrice, double albumPrice) {
+    public Track(String name, String genre, String artist, String album, String date, double trackPrice, double albumPrice) {
         this.name = name;
         this.genre = genre;
         this.artist = artist;
         this.album = album;
         this.trackPrice = trackPrice;
         this.albumPrice = albumPrice;
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
     }
 
     public String getName() {
@@ -65,8 +76,9 @@ public class Track {
                 ", genre='" + genre + '\'' +
                 ", artist='" + artist + '\'' +
                 ", album='" + album + '\'' +
-                ", trackPrice='" + trackPrice + '\'' +
-                ", albumPrice='" + albumPrice + '\'' +
+                ", trackPrice=" + trackPrice +
+                ", albumPrice=" + albumPrice +
+                ", date='" + date + '\'' +
                 '}';
     }
 }
