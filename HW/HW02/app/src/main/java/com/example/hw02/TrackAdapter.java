b80d52daade19fc5cc98a4692369cb20d186d0d1
Nikko Osaka <nikko.osaka99@gmail.com>
2020-02-25 12:11:10 -0500

    track adapter + layout

---
+++ HW/HW02/app/src/main/java/com/example/hw02/TrackAdapter.java
@@ -0,0 +1,46 @@
+package com.example.hw02;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class TrackAdapter extends ArrayAdapter<Track> {
+
+    public TrackAdapter(@NonNull Context context, int resource, @NonNull List<Track> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Track track = getItem(position);
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.track_item, parent, false);
+        }
+        TextView trackLabel = convertView.findViewById(R.id.track_label);
+        TextView artistLabel = convertView.findViewById(R.id.artist_label);
+        TextView priceLabel = convertView.findViewById(R.id.price_label);
+        TextView dateLabel = convertView.findViewById(R.id.date_label);
+
+        String trackLabelText = "Track: " + track.getName();
+        String artistLabelText = "Artist: " + track.getArtist();
+        String priceLabelText = "Price: $" + track.getTrackPrice();
+        String dateLabelText = "Date: " + track.getDate();
+
+        trackLabel.setText(trackLabelText);
+        artistLabel.setText(artistLabelText);
+        priceLabel.setText(priceLabelText);
+        dateLabel.setText(dateLabelText);
+
+        return convertView;
+    }
+}
