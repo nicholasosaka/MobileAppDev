56c3321c9b996f1d6dd6f2d166105095871bbdcc
Nikko Osaka <nikko.osaka99@gmail.com>
2020-03-18 20:24:58 -0400

    create MyNote layout

--- Exams/Midterm/app/src/main/java/com/example/midterm/MyNotesActivity.java
+++ Exams/Midterm/app/src/main/java/com/example/midterm/MyNotesActivity.java
@@ -1,14 +1,28 @@
 package com.example.midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MyNotesActivity extends AppCompatActivity {
+
+    ImageView logout;
+    ImageView add;
+    TextView nameTV;
+    RecyclerView recyclerView;
+
+    Context context;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_notes);
+        setTitle("My Notes");
     }
 }
